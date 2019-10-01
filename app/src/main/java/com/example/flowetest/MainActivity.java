c2c55c0d9a9304b57b6ed8351ffc2427efbf5734
mineyev2 <mineyev2@illinois.edu>
2019-09-30 21:35:34 -0500

    hello

--- app/src/main/java/com/example/flowetest/MainActivity.java
+++ app/src/main/java/com/example/flowetest/MainActivity.java
@@ -1,6 +1,22 @@
-package com.example.flowetest;
+package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+//import be.tarsos.dsp.io.android.;
+import be.tarsos.dsp.AudioDispatcher;
+import be.tarsos.dsp.AudioEvent;
+import be.tarsos.dsp.AudioProcessor;
+import be.tarsos.dsp.io.android.AndroidAudioPlayer;
+import be.tarsos.dsp.io.android.AudioDispatcherFactory;
+import be.tarsos.dsp.util.fft.FFT;
+
+import be.tarsos.dsp.pitch.PitchDetectionHandler;
+import be.tarsos.dsp.pitch.PitchDetectionResult;
+import be.tarsos.dsp.pitch.PitchProcessor;
+//import be.tarsos.dsp.AudioDispatcher;
+//import be.tarsos.dsp.pitch.PitchE;
+
+import be.tarsos.dsp.AudioEvent;
+import be.tarsos.dsp.AudioProcessor;
 
 import android.os.Bundle;
 
@@ -10,5 +26,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        PitchDetectionHandler pdh = new PitchDetectionHandler() {
+            @Override
+            public void handlePitch(PitchDetectionResult res, AudioEvent e){
+                final float pitchInHz = res.getPitch();
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        processPitch(pitchInHz);
+                    }
+                });
+            }
+        };
+        AudioProcessor pitchProcessor = new PitchProcessor(PitchProcessor.PitchEstimationAlgorithm.FFT_YIN, 22050, 1024, pdh);
+        dispatcher.addAudioProcessor(pitchProcessor);
+
+        Thread audioThread = new Thread(dispatcher, "Audio Thread");
+        audioThread.start();
     }
 }
